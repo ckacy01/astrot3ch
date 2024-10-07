20a5321954b096d46f2e1d5f7f855111fb0d996d
xGeorgeZ <jorgesr10@outlook.com>
2024-10-06 21:25:24 -0700

    add

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
-// Images and sound
-const imagen = document.getElementById('imagen');
-const audio = document.getElementById('audio');
+const images = document.querySelectorAll('.image-container');
+images.forEach((imageContainer, index) => {
+    const audio = imageContainer.querySelector('audio');
+    const muteButton = imageContainer.querySelector('.mute-button');
 
-imagen.addEventListener('mouseover', () => {
-    audio.play();
-});
+    imageContainer.addEventListener('mouseover', () => {
+        audio.play();
+    });
 
-imagen.addEventListener('mouseout', () => {
-    audio.pause();
-    audio.currentTime = 0; // Reloads the audio
-});
+    imageContainer.addEventListener('mouseout', () => {
+        audio.pause();
+        audio.currentTime = 0; // Reinicia el audio
+    });
 
-audio.addEventListener('ended', () => {
-    audio.currentTime = 0; 
-    audio.play(); 
-});
+    audio.addEventListener('ended', () => {
+        audio.currentTime = 0; 
+        audio.play(); 
+    });
 
-// Mute button
-const muteButton = document.getElementById('muteButton');
-muteButton.addEventListener('click', () => {
-    audio.muted = !audio.muted;
-    muteButton.textContent = audio.muted ? 'Unmute' : 'Mute';
+    muteButton.addEventListener('click', () => {
+        audio.muted = !audio.muted;
+        muteButton.textContent = audio.muted ? 'Unmute' : 'Mute';
+    });
 });
 
 // Navbar change color
@@ -32,3 +32,32 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('scrolled');
     }
 });
+
+
+/* pop up*/
+// Obtener los elementos
+var popup = document.getElementById('popup');
+var popupImg = document.getElementById('popup-img');
+var popupInfo = document.getElementById('popup-info');
+var close = document.getElementsByClassName('close')[0];
+
+// Añadir evento de clic a las imágenes
+document.querySelectorAll('.thumbnail').forEach(img => {
+    img.addEventListener('click', function() {
+        popup.style.display = 'block';
+        popupImg.src = this.src;
+        popupInfo.textContent = this.getAttribute('data-info');
+    });
+});
+
+// Cerrar el pop-up
+close.onclick = function() {
+    popup.style.display = 'none';
+}
+
+// Cerrar el pop-up al hacer clic fuera de él
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = 'none';
+    }
+}
